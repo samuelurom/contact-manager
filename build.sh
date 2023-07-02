528e0c49e58105e832c2371d9caba7443bc13219
Samuel Urom <urom.samuel@gmail.com>
2023-07-03 01:55:19 +1000

    Added configurations necessary for deployment

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 pip install -r requirements.txt
 python3.9 manage.py migrate
+mkdir -p staticfiles/dist
 python3.9 manage.py collectstatic --noinput
--- contactmgr/settings.py
+++ contactmgr/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KAY")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -135,11 +135,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
